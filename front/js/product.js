331bcd483244443f6b1ed0a11c6e48b46ea15304
gilles <gt40370@outlook.fr>
2022-03-19 21:22:39 +0100

    modification nom des make

--- front/js/product.js
+++ front/js/product.js
@@ -19,15 +19,15 @@ function handleData(canaper) {
   //   const price = canaper.price
   //   const _id = canaper._id
   const { altTxt, colors, description, imageUrl, name, price } = canaper;
-  makeImage(imageUrl, altTxt);
-  makeTitle(name);
-  makePrice(price);
-  makeDescription(description);
-  makeMenucolors(colors);
+  faireImage(imageUrl, altTxt);
+  faireTitle(name);
+  fairePrice(price);
+  faireDescription(description);
+  faireMenuColors(colors);
 }
 
 // tous se qui concerne l'image et la balise img rajouter as la div avec la classe item__img
-function makeImage(imageUrl, altTxt) {
+function faireImage(imageUrl, altTxt) {
   const image = document.createElement("img");
   image.src = imageUrl;
   image.altTxt = altTxt;
@@ -37,31 +37,30 @@ function makeImage(imageUrl, altTxt) {
 
 // sa ses tous se contient la div title et la balise h1 existant deja ont pas besoin de cree un nouvelle element
 //  ont selectionne juste l'id #title et as l aide de textContent ont rajoute le contenu la ses se qui es marquier dans l api as name.
-function makeTitle(name) {
+function faireTitle(name) {
   const h1 = document.querySelector("#title");
   if (h1 != null) h1.textContent = name;
 }
 // ses la meme chose que au dessus la sa concerne la span   sauf que la sa concerne le prix(price)
-function makePrice(price) {
+function fairePrice(price) {
   const span = document.querySelector("#price");
   if (span != null) span.textContent = price;
 }
 // la meme que au dessus mais la sa concerne la balise p ou il y la description
-function makeDescription(description) {
+function faireDescription(description) {
   const p = document.querySelector("#description");
   if (p != null) p.textContent = description;
 }
 // la sa concerne le menu deroulent des differentes couleurs
 // que l API propose
-function makeMenucolors(colors) {
+function faireMenuColors(colors) {
   const select = document.querySelector("#colors");
   if (select != null) {
     /*le select ses pour avoir plusieur choix */
     colors.forEach((color) => {
       /*foreach sa permety d'executer la function donner sur le tableaux (array)*/
       const option = document.createElement("option");
-      option.value =
-        color; /*value permet le renvois des des valeur de la fonction dans l ordre quelle sont sur le tableaux exemple 
+      option.value =color; /*value permet le renvois des des valeur de la fonction dans l ordre quelle sont sur le tableaux exemple 
       rouge bleue vert sa peut pas etre vert bleue rouge*/
       option.textContent = color;
       select.appendChild(option);
