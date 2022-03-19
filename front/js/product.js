0b0d02a19afe3dd9aa33866ca99177738b173b0d
gilles <gt40370@outlook.fr>
2022-03-19 18:21:32 +0100

    page products

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,70 @@
 const queryString = window.location.search;
 // console.log(queryString);
-const urlParams = new URLSearchParams(queryString)
+const urlParams = new URLSearchParams(queryString);
 // console.log(urlParams)
-const id = urlParams.get("id")
+const id = urlParams.get("id");
 // console.log({ id })
 
-fetch('http://localhost:3000/api/products/${id}')
-.then((response) => response.json())
-.then((res) => console.log(res))
+// sa log tout se qu'il reçois de l api concernant l'article qui as etais clicker
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then((response) => response.json())
+  .then((res) => handleData(res));
+
+function handleData(canaper) {
+  //   const altTxt = canaper.altTxt
+  //   const colors = canaper.colors
+  //   const description = canaper.description
+  //   const imageUrl = canaper.imageUrl
+  //   const name = canaper.name
+  //   const price = canaper.price
+  //   const _id = canaper._id
+  const { altTxt, colors, description, imageUrl, name, price } = canaper;
+  makeImage(imageUrl, altTxt);
+  makeTitle(name);
+  makePrice(price);
+  makeDescription(description);
+  makeMenucolors(colors);
+}
+
+// tous se qui concerne l'image et la balise img rajouter as la div avec la classe item__img
+function makeImage(imageUrl, altTxt) {
+  const image = document.createElement("img");
+  image.src = imageUrl;
+  image.altTxt = altTxt;
+  const parents = document.querySelector(".item__img");
+  if (parents != null) parents.appendChild(image);
+}
+
+// sa ses tous se contient la div title et la balise h1 existant deja ont pas besoin de cree un nouvelle element
+//  ont selectionne juste l'id #title et as l aide de textContent ont rajoute le contenu la ses se qui es marquier dans l api as name.
+function makeTitle(name) {
+  const h1 = document.querySelector("#title");
+  if (h1 != null) h1.textContent = name;
+}
+// ses la meme chose que au dessus la sa concerne la span   sauf que la sa concerne le prix(price)
+function makePrice(price) {
+  const span = document.querySelector("#price");
+  if (span != null) span.textContent = price;
+}
+// la meme que au dessus mais la sa concerne la balise p ou il y la description
+function makeDescription(description) {
+  const p = document.querySelector("#description");
+  if (p != null) p.textContent = description;
+}
+// la sa concerne le menu deroulent des differentes couleurs
+// que l API propose
+function makeMenucolors(colors) {
+  const select = document.querySelector("#colors");
+  if (select != null) {
+    /*le select ses pour avoir plusieur choix */
+    colors.forEach((color) => {
+      /*foreach sa permety d'executer la function donner sur le tableaux (array)*/
+      const option = document.createElement("option");
+      option.value =
+        color; /*value permet le renvois des des valeur de la fonction dans l ordre quelle sont sur le tableaux exemple 
+      rouge bleue vert sa peut pas etre vert bleue rouge*/
+      option.textContent = color;
+      select.appendChild(option);
+    });
+  }
+}
